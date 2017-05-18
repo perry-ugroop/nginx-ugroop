e743dd925bc684d5107324574221b2554ef7548e
Perry Valdez <perry.valdez@ugroop.com.au>
2017-05-18 14:06:15 +1000

    Updated startup

--- startup.sh
+++ startup.sh
@@ -5,6 +5,6 @@ if [[ -d /etc/nginx.default ]] ; then
     rm -fR /etc/nginx.default/
 fi
 
-echo '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Ping</title></head><body><p>Hello, fellas!</p></body></html>' > /usr/share/nginx/html/ping.html
+echo '<!DOCTYPE html><html><head><meta charset="utf-8" /><title>Ping</title></head><body><p>Hello, people!</p></body></html>' > /usr/share/nginx/html/ping.html
 nginx -g 'daemon off;'
 
